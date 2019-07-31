97c196dd32f104e5ae150d0e3a2bcce1169369bd
Majid LAHMIDI <alahmidi@ausy-group.com>
2019-07-31 19:48:11 +0200

    remove nuxeo

--- tests/nuxeo/nuxeo-server-tomcat-9.10-HF19/bin/stream.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ROOT=$(cd $(dirname $0)/..; pwd -P)
-OG4J_CONF=tools-log4j.xml
-CLASSPATH="$ROOT/lib/*:$ROOT/nxserver/lib/*:$ROOT/nxserver/bundles/*"
-cd $ROOT
-java -cp $CLASSPATH org.nuxeo.lib.stream.tools.Main "$@"
